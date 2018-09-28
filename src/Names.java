5b9a0dd0cd818a0dc3b680692b4afa15786b8e5b
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-28 14:29:43 +0200

    Kolejna wersja

--- src/Names.java
+++ src/Names.java
@@ -1,22 +1,16 @@
 import java.util.Scanner;
+
 public class Names {
+    public static void main(String[] args){
+        String[] tab = new String[5];
 
-    public static void main(String[]args){
-
-        String[] names = new String[5];
         Scanner imiona = new Scanner(System.in);
-        System.out.println("Podaj pierwsze imię:");
-        names[0]=imiona.nextLine();
-        System.out.println("Podaj drugie imię:");
-        names[1]=imiona.nextLine();
-        System.out.println("Podaj trzecie imię:");
-        names[2]=imiona.nextLine();
-        System.out.println("Podaj czwarte imię:");
-        names[3]=imiona.nextLine();
-        System.out.println("Podaj piąte imię:");
-        names[4]=imiona.nextLine();
 
-        System.out.println("Cześć "+names[4]+"!"+"\nCześć "+names[3]+"!"+"\nCześć "+names[2]+"!"+"\nCześć "+names[1]+"!"+"\nCześć "+names[0]+"!");
+        for(int i=0; i<5; i++)
+            tab[i] = imiona.nextLine();
 
+        for(int i=0; i<5; i++)
+            System.out.println("Cześć "+tab[i]);
     }
 }
+
