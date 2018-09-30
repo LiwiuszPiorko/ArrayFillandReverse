32207bbd02fc4269452f8018b00accd85b9c1438
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-30 08:41:38 +0200

    Ostateczna wersja

--- src/Names.java
+++ src/Names.java
@@ -5,10 +5,10 @@ public class Names {
         String[] tab = new String[5];
         Scanner imiona = new Scanner(System.in);
         System.out.println("Wpisz pięć przypadkowych imion:");
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < tab.length; i++)
             tab[i] = imiona.nextLine();
 
-        for (int i = 0; i < 5; i++)
+        for (int i = tab.length-1; i>=0; i--)
             System.out.println("Cześć " + tab[i]);
     }
 }
